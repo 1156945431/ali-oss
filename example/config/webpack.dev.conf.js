dfda09b672c6d7bb1f1b2b97015e3b8b331b7e6b
jingdan <binghaiwang@gmail.com>
2018-03-15 15:44:16 +0800

    feat: browser structure with es7

--- example/config/webpack.dev.conf.js
+++ example/config/webpack.dev.conf.js
@@ -1,39 +1,39 @@
-const path = require('path')
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack')
+const webpack = require('webpack');
 
 const HtmlWebpackPluginConfig = {
   filename: 'index.html',
-  template: './template/index.html', 
+  template: './template/index.html',
   inject: 'body',
-}
+};
 
 module.exports = {
-  context: path.resolve(__dirname,'../src'),
+  context: path.resolve(__dirname, '../src'),
   entry: './main',
-  output:{
-    path: path.resolve(__dirname,'../dist'),
+  output: {
+    path: path.resolve(__dirname, '../dist'),
     filename: './[hash]app.js',
-    hashDigestLength: 8
+    hashDigestLength: 8,
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+        use: ['style-loader', 'css-loader'],
       },
-      { 
-        test: /\.(eot|woff|woff2)$/, 
-        loader: "file-loader" 
+      {
+        test: /\.(eot|woff|woff2)$/,
+        loader: 'file-loader',
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-        }
-      }
-    ]
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin(HtmlWebpackPluginConfig),
@@ -41,17 +41,17 @@ module.exports = {
       $: 'jquery',
       jQuery: 'jquery',
       'window.jQuery': 'jquery',
-      Popper: ['popper.js', 'default']
-    })
-  ], 
+      Popper: ['popper.js', 'default'],
+    }),
+  ],
   devServer: {
-    contentBase: path.resolve(__dirname, "../dist"),
+    contentBase: path.resolve(__dirname, '../dist'),
     port: 3000,
     host: '0.0.0.0',
     open: true, // open browser auto
     index: 'index.html', // like HtmlWebpackPlugin
     inline: true, // default:true
     hot: false,
-    compress: true // compress
-  }
-}
+    compress: true, // compress
+  },
+};
